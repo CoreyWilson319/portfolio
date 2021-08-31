a6f879f0a6f7fdb61ba6a24f4939ec330fc2f433
Coreywilson319 <coreywilson319@gmail.com>
2021-08-31 15:14:46 -0400

    more styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -40,18 +40,6 @@ const Home = () => {
           </a>
         </li>
       </ul>
-      <p id="about">
-        I am a graduate of General Assembly's Software Engineer Immersive
-        program. After going some time of trying to self-teach myself to become
-        a developer, and losing my job due to covid-19 opened the door for me to
-        take the time to participate in this program. During this program I
-        improved my understanding of the basics of coding as well as taking that
-        knowledge further and developing full stack web applications. I believe
-        that I am ready to take the leap to the next step and become a
-        professional software engineer. I am soft spoken and reserved which
-        allow me to work well with a team and individually. I am a good problem
-        solver and am always trying to think ahead and possible outcomes.
-      </p>
 
       <div class="projects">
         <div id="card">
@@ -100,6 +88,18 @@ const Home = () => {
           </Card>
         </div>
       </div>
+      <p id="about">
+        I am a graduate of General Assembly's Software Engineer Immersive
+        program. After going some time of trying to self-teach myself to become
+        a developer, and losing my job due to covid-19 opened the door for me to
+        take the time to participate in this program. During this program I
+        improved my understanding of the basics of coding as well as taking that
+        knowledge further and developing full stack web applications. I believe
+        that I am ready to take the leap to the next step and become a
+        professional software engineer. I am soft spoken and reserved which
+        allow me to work well with a team and individually. I am a good problem
+        solver and am always trying to think ahead and possible outcomes.
+      </p>
     </div>
   );
 };
